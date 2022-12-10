1a74869aeefea69d6d6036c79744e4c7288bc96c
Fabdulelah <118623940+Fabdulelah@users.noreply.github.com>
2022-12-10 16:21:36 +0300

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("مرحباً بك في موقعي! انا (فاطمة)، وسوف احسب درجتك");
+let grade = prompt ( "enter your grade here ") ;
+console.log(grade)
+
+if(  grade >= 90 && grade <=100) { 
+
+    console.log ("لقد حصلت على امتياز ") 
+} else if ( grade>=80 grade &&<=89){
+    console.log("لقد حصلت على جيد جد")
+} else if ( )
+    
